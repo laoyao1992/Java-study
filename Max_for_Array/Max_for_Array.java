ea303e4fed1850a676ddd2623c19e0aa9af4288e
laoyao1992 <1543460799@qq.com>
2019-12-22 22:05:07 +0800

    Max for array

---
+++ Max_for_Array/Max_for_Array.java
@@ -0,0 +1,15 @@
+class ArrayDemo4
+{
+	public static void main(String[] args)
+	{
+		int[] arr = {10, 12, 15, 5, 20};
+		int max = getMax(arr);
+		System.out.println("max="+max);
+	}
+	
+	public static int getMax(int[] arr)
+	{
+		
+	}
+}
+
