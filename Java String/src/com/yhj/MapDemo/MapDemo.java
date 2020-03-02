2c7a39e8f31e87d87bbe1afe9d3fc94aac5c351e
laoyao1992 <1543460799@qq.com>
2020-03-02 21:13:19 +0800

    map demo

--- Java String/src/com/yhj/MapDemo/MapDemo.java
+++ Java String/src/com/yhj/MapDemo/MapDemo.java
@@ -9,6 +9,16 @@ public class MapDemo {
 		Map<Integer,String> map = new HashMap<Integer,String>();
 		method(map);
 	}
-	
+	public static void method(Map<Integer, String> map) {
+		System.out.println(map.put(8, "wangcai"));
+		System.out.println(map.put(8, "xiaozhang"));
+		map.put(2, "xiaohua");
+		map.put(3, "xiaoxia");
+		
+		System.out.println("get:"+map.get(8));
+		
+		System.out.println(map);
+		
+	}
 	
 }
